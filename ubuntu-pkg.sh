9327f55895f95ffb84f9c834cb5bc6da75c8e384
Cloud Cafe <68260844+cloudcafetech@users.noreply.github.com>
2024-02-12 08:06:36 +0530

    Update ubuntu-pkg.sh

--- ubuntu-pkg.sh
+++ ubuntu-pkg.sh
@@ -4,7 +4,7 @@
 # Can be typically run in docker to select the version you want like:
 # docker run -it -v $(pwd):/host ubuntu:20.04 bash -c "$(cat ./this_script.sh)"
 
-PACKAGES="apt-transport-https ca-certificates gpg nfs-common curl wget git net-tools unzip jq zip nmap telnet dos2unix apparmor ldap-utils"
+PACKAGES="apt-transport-https ca-certificates gpg nfs-common nfs-kernel-server curl wget git net-tools unzip jq zip nmap telnet dos2unix apparmor ldap-utils"
 apt update; apt install -y apt-rdepends lsb-release
 cd /tmp
 # Download all dependencies + the package itself
