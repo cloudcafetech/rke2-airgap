0b3c9f96be60dbbca0c7cae207b42a1cbe0f5fc6
Cloud Cafe <68260844+cloudcafetech@users.noreply.github.com>
2024-02-28 18:28:18 +0530

    Update ubuntu-kubeadm-crio-pkg.sh

--- ubuntu-kubeadm-crio-pkg.sh
+++ ubuntu-kubeadm-crio-pkg.sh
@@ -5,9 +5,9 @@
 # docker run -it -e K8S=1.26 -v $(pwd):/host ubuntu:20.04 bash -c "$(cat ./this_script.sh)"
 
 # For crio
-PACKAGES=(kubectl kubelet kubernetes-cni kubeadm apt-transport-https ca-certificates curl openssl selinux-utils cri-o cri-o-runc podman cri-tools socat ebtables conntrack gpg nmap) 
+PKGCRIOS=(kubectl kubelet kubernetes-cni kubeadm apt-transport-https ca-certificates curl openssl selinux-utils cri-o cri-o-runc podman cri-tools socat ebtables conntrack gpg nmap) 
 # For containerd 
-#PACKAGES=(kubectl kubelet kubernetes-cni kubeadm apt-transport-https ca-certificates curl openssl libapparmor1 libc6 perl liberror-perl git-man less selinux-utils containerd.io docker-ce docker-ce-cli cri-tools socat ebtables conntrack gpg nfs-common nfs-kernel-server wget git net-tools unzip jq zip nmap telnet dos2unix apparmor ldap-utils) 
+PKGCONDS=(kubectl kubelet kubernetes-cni kubeadm apt-transport-https ca-certificates curl openssl libapparmor1 libc6 perl liberror-perl git-man less selinux-utils containerd.io docker-ce docker-ce-cli cri-tools socat ebtables conntrack gpg nmap) 
 
 OS=xUbuntu_20.04
 VERSION=1.26
@@ -23,20 +23,29 @@ mkdir -p /etc/apt/keyrings
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v${K8S}/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v$K8S/deb/ / | tee /etc/apt/sources.list.d/kubernetes.list
 
+# For crio
 curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | gpg --dearmor -o /etc/apt/keyrings/libcontainers-archive-keyring.gpg
 curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/Release.key | gpg --dearmor -o /etc/apt/keyrings/libcontainers-crio-archive-keyring.gpg
 echo "deb [signed-by=/etc/apt/keyrings/libcontainers-archive-keyring.gpg] https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /" | tee /etc/apt/sources.list.d/devel-kubic-libcontainers-stable.list
 echo "deb [signed-by=/etc/apt/keyrings/libcontainers-crio-archive-keyring.gpg] https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/ /" | tee /etc/apt/sources.list.d/devel-kubic-libcontainers-stable-crio-$VERSION.list
 
-# For containerd enable below
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-#add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
+# For containerd 
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
 
 apt update
 
 cd /tmp
-# Download all dependencies + the package itself
-for PACKAGE in ${PACKAGES[@]}; do  apt-get -y -o Dir::Cache::Archives=./ install --download-only --reinstall $PACKAGE; done
-# Get them back to the host
+mkdir crio cond
+# Download all dependencies CONTAINERD package 
+cd /tmp/cond
+for PKGCOND in ${PKGCONDS[@]}; do  apt-get -y -o Dir::Cache::Archives=./ install --download-only --reinstall $PKGCOND; done
 rm -rf containernetworking-plugins*.deb
-cp /tmp/*.deb /host/
+
+# Download all dependencies CRIO package 
+cd /tmp/crio
+for PKGCRIO in ${PKGCRIOS[@]}; do  apt-get -y -o Dir::Cache::Archives=./ install --download-only --reinstall $PKGCRIO; done
+rm -rf containernetworking-plugins*.deb
+
+# Get them back to the host
+cp -rf /tmp/c* /host/
